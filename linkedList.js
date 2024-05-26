8964f7beb27578435d432c73e455981045826b20
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-26 08:58:53 +0000

    Add append(value) function to LinkedList class

--- linkedList.js
+++ linkedList.js
@@ -6,7 +6,20 @@ class Node {
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      return;
     }
+    let current = this.head;
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+    current.nextNode = newNode;
+  }
 }
